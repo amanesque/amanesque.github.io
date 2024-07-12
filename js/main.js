423e7b61038c8a43f0464a5941aa7e73c11c0c40
thebenzo <amanvrma5799@gmail.com>
2024-07-12 22:25:28 +0530

    Create hero section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-function sayHello() {
-    alert("Hello");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const { annotate } = window.RoughNotation;
+
+  const names = document.querySelectorAll('.name');
+  const links = document.querySelectorAll('.annotated-link');
+
+  names.forEach(name => {
+    const nameAnnotation = annotate(name, { type: 'highlight', color: '#17B89080' });
+    nameAnnotation.show();
+  });
+
+  links.forEach(link => {
+    const annotation = annotate(link, { type: 'box', padding: [1, 1], color: '#14A380', animationDuration: 600 });
+
+    link.addEventListener('mouseenter', () => {
+      annotation.show();
+    });
+
+    link.addEventListener('mouseleave', () => {
+      annotation.hide();
+    });
+  });
+});
